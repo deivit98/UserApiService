d553f229734d2e49c9b8837802a51da922639003
deyvit <azazel7298@gmail.com>
2024-07-23 14:53:13 +0300

    added unit tests

---
+++ UserData.DataAccess.UnitTests/UserRepositoryTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserData.DataAccess.Models;
+using UserData.DataAccess.Repositories;
+
+namespace UserData.DataAccess.UnitTests
+{
+    public class UserRepositoryTests
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IUserRepository _sut;
+
+        public UserRepositoryTests()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+            _context = new ApplicationDbContext(options);
+            _context.Database.EnsureCreated();
+
+            AddUsersToDatabase(_context);
+
+            _sut = new UserRepository(_context);
+        }
+
+        private void AddUsersToDatabase(ApplicationDbContext context)
+        {
+            _context.Users.AddRange(
+                new User { Id = Guid.NewGuid(), UserName = "ivivan", DisplayName = "Ivan Ivanov", Email = "i.ivanov@example.com" },
+                new User { Id = Guid.NewGuid(), UserName = "igeor", DisplayName = "Ivelina Georgieva", Email = "i.georgieva@example.com" },
+                new User { Id = Guid.NewGuid(), UserName = "kivanova", DisplayName = "Katerina Ivanova", Email = "k.ivanova@example.com" },
+                new User { Id = Guid.NewGuid(), UserName = "igerd", DisplayName = "Ivaila Gerdjikova", Email = "i.gerdjikova@example.com" },
+                new User { Id = Guid.NewGuid(), UserName = "kakost", DisplayName = "Kaloqn Kostadinov", Email = "k.kostadinov@example.com" },
+                new User { Id = Guid.NewGuid(), UserName = "gdimit", DisplayName = "Georgi Dimitrov", Email = "g.dimitrov@example.com" });
+        }
+    }
+}
---
+++ UserData.Service.UnitTests/UserServiceTests.cs
@@ -0,0 +1,66 @@
+﻿using Moq;
+using UserApplication.Service.Services;
+using UserData.DataAccess.Models;
+using UserData.DataAccess.Repositories;
+using UserData.Service.Services;
+
+namespace UserData.Service.UnitTests
+{
+    public class UserServiceTests
+    {
+        private readonly Mock<IUserRepository> _userRepo;
+        private readonly IUserService _sut;
+
+        public UserServiceTests()
+        {
+            _userRepo = new Mock<IUserRepository>();
+
+            _sut = new UserService(_userRepo.Object);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public async Task GetUsers_WithEmtyStringInput_ReturnEmptyList(string? input)
+        {
+            // Arrange
+
+            // Act
+            var result = await _sut.GetUsers(input);
+
+            // Assert
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public async Task GetUsers_WithFullName_ShouldReturnOneResult()
+        {
+            // Arrange
+            var input = "g";
+
+            var serviceResult = new List<User>();
+
+            var expectedModelToBeReturned = new User()
+            {
+                Id = Guid.NewGuid(),
+                DisplayName = "Georgi",
+                Email = "g.someone@example.com",
+                UserName = "gsomeone"
+            };
+
+            serviceResult.Add(expectedModelToBeReturned);
+
+            _userRepo.Setup(x => x.GetUsers(input)).ReturnsAsync(serviceResult);
+
+            // Act
+            var result = await _sut.GetUsers(input);
+
+            var listResult = result.ToList();
+
+            // Arrange
+            Assert.NotNull(result);
+            Assert.Equal(serviceResult.Count, listResult.Count);
+            Assert.Equal(serviceResult[0].DisplayName, listResult[0].DisplayName);
+        }
+    }
+}
